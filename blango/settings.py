8966ca49a5e5dd8d4405a18bc7cec1a737a75cce
Marco Parra <maaferna@gmail.com>
2022-04-15 16:28:08 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
