164a39da367137f12c7e5947dd9490f492e86ab9
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2022-12-26 05:28:16 +0000

    Finish html frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path("", blog.views.index),
 ]
