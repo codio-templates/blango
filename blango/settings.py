6fa14a9be30966d592c174f305c7036f404fa8f8
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2023-01-02 03:43:27 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -33,6 +33,8 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 # Application definition
@@ -45,6 +47,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "crispy_forms",
+    "crispy_bootstrap5",
+
 ]
 
 MIDDLEWARE = [
---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from blog.models import Comment
+from crispy_forms.layout import Submit
+from crispy_forms.helper import FormHelper
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))        
