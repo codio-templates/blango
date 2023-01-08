edde8fd7c0153bef9a6b49877052db0db0ba4763
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2023-01-08 17:25:21 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -141,6 +141,46 @@ class Dev(Configuration):
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+    LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+            "filters": ["require_debug_false"],
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 from django.shortcuts import render
@@ -5,9 +6,11 @@ from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 
+logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
@@ -21,6 +24,9 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+)
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
