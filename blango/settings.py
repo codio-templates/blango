5110b4a6e238c9e0ef73feefeb1dbfa3dfb4e1da
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2023-01-18 16:49:01 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -51,12 +51,17 @@ class Dev(Configuration):
         "django.contrib.contenttypes",
         "django.contrib.sessions",
         "django.contrib.messages",
+        "django.contrib.sites"
         "django.contrib.staticfiles",
         "blango_auth",
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
         "debug_toolbar",
+		"allauth",
+		"allauth.account",
+		"allauth.socialaccount",
+		"allauth.socialaccount.providers.google",
 
     ]
 
@@ -151,6 +156,12 @@ class Dev(Configuration):
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+    
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     LOGGING = {
     "version": 1,
--- blango/urls.py
+++ blango/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
         name="django_registration_register",
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
 
 if settings.DEBUG:
