3b8aa3f20208d4da2a7c2e58d8cb85a8e5dfcc30
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2023-01-24 16:37:51 +0000

    Finish Serializers

---
+++ blog/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from blog.models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        readonly = ["modified_at", "created_at"]
