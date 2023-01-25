febb095c57e97d095631ab8b553d78b44f76827b
Jonathan Sevilla <jonathansevilla.7@gmail.com>
2023-01-25 03:50:32 +0000

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from blog.models import Tag, Post
+
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
