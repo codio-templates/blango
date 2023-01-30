e2730f57a79b60f0ecf8dd3c501f343dddf740c8
Martin yacoub <abomilad7323@gmail.com>
2023-01-30 09:23:59 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post , Comment
 # Register your models here.
 
-admin.site.register(Tag)
+
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     #list_display = ('slug', 'published_at')
     list_display = ('content', 'title')
 
-admin.site.register(Post, PostAdmin)    
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+
--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,34 @@
 from django.db import models
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 from django.db import models
 from django.conf import settings
+
+from django.contrib.contenttypes.fields import GenericRelation
 
 class Tag(models.Model):
     value = models.TextField(max_length=100)
 
     def __str__(self):
         return self.value
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    # above method will let them both be null or have a vlue at the same time 
+    # and that wrong so we will change
+    #to let just point to one of them
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    #content_object = GenericForeignKey()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -19,7 +39,11 @@ class Post(models.Model):
     slug = models.SlugField()
     summary = models.TextField(max_length=500)
     content = models.TextField()
+    
     tags = models.ManyToManyField(Tag, related_name="posts")
+
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
+
