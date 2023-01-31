b210314948393cccaba5c90a90e15039509aa23b
Martin yacoub <abomilad7323@gmail.com>
2023-01-31 07:41:50 +0000

    Finish html & bootstrap  frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import blog.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", blog.views.index)
+
 ]
