8362b0c46fed413220f2e79a7d89875ed37cb5f5
Martin yacoub <abomilad7323@gmail.com>
2023-02-14 09:23:16 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -56,6 +56,7 @@ class Dev(Configuration):
       'allauth.account',
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
+      'rest_framework',
 
       
   ]
--- blog/api_views.py
+++ blog/api_views.py
@@ -8,30 +8,18 @@ from django.views.decorators.csrf import csrf_exempt
 
 from blog.models import Post
 
-
-def post_to_dict(post):
-    return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-    }
-
+from blog.api.serializers import PostSerializer
 
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": posts_as_dict})
+        return JsonResponse({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
         post_data = json.loads(request.body)
-        post = Post.objects.create(**post_data)
+        serializer = PostSerializer(data=post_data)
+        serializer.is_valid(raise_exception=True)
+        post = serializer.save()
         return HttpResponse(
             status=HTTPStatus.CREATED,
             headers={"Location": reverse("api_post_detail", args=(post.pk,))},
@@ -45,12 +33,12 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
+        return JsonResponse(PostSerializer(post).data)
     elif request.method == "PUT":
         post_data = json.loads(request.body)
-        for field, value in post_data.items():
-            setattr(post, field, value)
-        post.save()
+        serializer = PostSerializer(post, data=post_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return HttpResponse(status=HTTPStatus.NO_CONTENT)
     elif request.method == "DELETE":
         post.delete()
