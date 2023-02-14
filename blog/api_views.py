e24c499c97086aafa0bee9ca8057267fb2f872ba
Martin yacoub <abomilad7323@gmail.com>
2023-02-14 11:32:25 +0000

    Generic views and mixins API

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,50 +1,29 @@
-from rest_framework import status
-
-from django.urls import reverse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
+from rest_framework import mixins
+from rest_framework import generics
 
-@api_view(["GET", "POST"])
-def post_list(request, format=None):
-    
-    if request.method == "GET":
-        posts = Post.objects.all()
-        return Response({"data": PostSerializer(posts, many=True).data})
-    
-    elif request.method == "POST":
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            post = serializer.save()
-            return Response(
-                status=HTTPStatus.CREATED,
-                headers={"Location": reverse("api_post_detail", args=(post.pk,))},
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-from rest_framework.views import APIView
+class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-class PostDetail(APIView):
-    @staticmethod
-    def get_post(pk):
-        return get_object_or_404(Post, pk=pk)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        post = self.get_post(pk)
-        return Response(PostSerializer(post).data)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        post = self.get_post(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=HTTPStatus.NO_CONTENT)
-        return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+class PostDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView,):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def delete(self, request, pk, format=None):
-        post = self.get_post(pk)
-        post.delete()
-        return Response(status=HTTPStatus.NO_CONTENT)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
