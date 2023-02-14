d0b441f8991a622cd4cf1b25932daf497565fbfd
Martin yacoub <abomilad7323@gmail.com>
2023-02-14 11:49:09 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -47,7 +47,7 @@ urlpatterns = [
     
     path("accounts/", include("django_registration.backends.activation.urls")),
     #path("accounts/", include("django_registration.backends.one_step.urls")),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
     
 ]
 
---
+++ blog/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+
+from blog.api.serializers import PostSerializer
+from blog.models import Post
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
