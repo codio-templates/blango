05cbde520945ff595f693ae610169dfd11f9c4bc
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2023-03-07 14:35:38 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,27 @@
-const theNumber = 1
-let  yourName = 'Ben'
-
-if (theNumber === 1) {
-  let  yourName = 'Leo'
-  alert(yourName)
+for(let i = 0; i < 10; i += 1) {
+  console.log('for loop i: ' + i)
 }
 
-alert(yourName)
+let j = 0
+while(j < 10) {
+  console.log('while loop j: ' + j)
+  j += 1
+}
+
+let k = 10
+
+do {
+  console.log('do while k: ' + k)
+} while(k < 10)
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numbers.forEach((value => {
+  console.log('For each value ' + value)
+}))
+
+const doubled = numbers.map(value => value * 2)
+
+console.log('Here are the doubled numbers')
+
+console.log(doubled)
