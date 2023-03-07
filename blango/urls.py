aad717d08d4cd53f508e6295a3ef9800925b5b64
REDHA ALKHULAQI <reda.ali.ahmed@gmail.com>
2023-03-07 04:33:37 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("allauth.urls")),
     path("api/v1/", include("blog.api.urls")),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),    
 ]
 
 if settings.DEBUG:
---
+++ blog/static/blog/blog.js
@@ -0,0 +1,9 @@
+const theNumber = 1
+let  yourName = 'Ben'
+
+if (theNumber === 1) {
+  let  yourName = 'Leo'
+  alert(yourName)
+}
+
+alert(yourName)
