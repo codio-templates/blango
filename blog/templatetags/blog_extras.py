642a24fabc99f8394a0b459df1113e6d1de9dfaa
Michał Pilch <michael.pilch88@gmail.com>
2023-03-23 16:57:18 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,33 @@
+
+from django.contrib.auth import get_user_model
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+user_model = get_user_model()
+register = template.Library()
+
+
+@register.filter
+def author_details(author, current_user):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html('{}{}{}', prefix, name, suffix)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from blog.models import Post
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
