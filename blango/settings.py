4461e0e4881a02804791a1878932505aa2f8bf00
Michał Pilch <michael.pilch88@gmail.com>
2023-03-23 13:24:36 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- blango/urls.py
+++ blango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blog.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", blog.views.index),
 ]
