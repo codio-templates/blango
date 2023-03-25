0a493f49fb1eacf1d34edfc75bd1245189446c91
Michał Pilch <michael.pilch88@gmail.com>
2023-03-25 16:18:35 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -109,7 +109,12 @@ class Dev(Configuration):
           'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
       },
   ]
-
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
 
   # Internationalization
   # https://docs.djangoproject.com/en/3.2/topics/i18n/
