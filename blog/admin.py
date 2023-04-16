5d1b92d6c31dbb081ec6b65e7da00c8ed8efe29d
winstonmhango23 <winstonmhango23@gmail.com>
2023-04-16 19:59:25 +0200

    added blog and ContentTypes

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post,  Comment
 # Register your models here.
 
 admin.site.register(Tag)
@@ -8,4 +8,7 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display =['slug', 'published_at']
 
+# Register your models here.
+admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
+
 
 # Create your models here.
 class Tag(models.Model):
@@ -7,6 +11,16 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
 
 class Post(models.Model):
@@ -19,6 +33,8 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
+
