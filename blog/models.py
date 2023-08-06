a5c55e51fb68d6fe1c611e1610cd2a61a4c0a27c
Venkata Ratnam bhumireddy <venkat4ratnam@gmail.com>
2023-08-06 12:45:37 +0000

    blog models-Post and Tag
    1) Added Post model
    2) Added Tag model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.value
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField()
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+
+    def __str__(self):
+        return self.title
