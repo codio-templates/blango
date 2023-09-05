5420964c4d6eb14c96ba03c29a2ca3aa7802dcaa
Abenezer Shiferaw <abenxy0@gmail.com>
2023-09-05 17:29:51 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     name="django_registration_register",
 ),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("api/v1/", include("blog.api_urls")),
+
 
 ]
 
