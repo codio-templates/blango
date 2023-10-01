4cd870290f00250fcad1a35f9c4628c13303a259
Abenezer Shiferaw <abenxy0@gmail.com>
2023-10-01 17:07:28 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+from datetime import timedelta
+
 
 
 
@@ -206,6 +208,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
         ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -241,6 +244,10 @@ class Dev(Configuration):
         }
     }
 
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
 
 class Prod(Dev):
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -7,6 +7,8 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register("tags", TagViewSet)
@@ -38,6 +40,9 @@ urlpatterns += [
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
+
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
