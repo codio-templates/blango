a6fa77a4d3e6292b60e81b0be47743329e282b28
Thiruvalluvan Murugesan <tiruvaalluvar.m@gmail.com>
2023-12-25 04:06:00 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DIRS: [BASE_DIR / 'templates']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+# # other imports
+# import blog.views
+
+# urlpatterns = [
+#     # other patterns
+#     path("", blog.views.index)
+# ]
