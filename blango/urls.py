e3bfd3939346e4481a7f983497912139969991b3
Tiru m <tiruvaalluvar.m@gmail.com>
2023-12-31 05:24:37 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -1,3 +1,5 @@
+import blog.views
+
 """blango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,10 +17,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", blog.views.index)
-    
+    path("", blog.views.index),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
