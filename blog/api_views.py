9309799ca5f90e63952df26e478d210e7be0015a
Thiruvalluvan Murugesan <tiruvaalluvar.m@gmail.com>
2024-01-03 14:53:34 +0000

    Finish Serializers

--- blog/api_views.py
+++ blog/api_views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from blog.models import Post
+from blog.api.serializers import PostSerializer
 
 
 def post_to_dict(post):
@@ -26,16 +27,13 @@ def post_to_dict(post):
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
-        posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": posts_as_dict})
+      posts = Post.objects.all()
+      return JsonResponse({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
-        post_data = json.loads(request.body)
-        post = Post.objects.create(**post_data)
-        return HttpResponse(
-            status=HTTPStatus.CREATED,
-            headers={"Location": reverse("api_post_detail", args=(post.pk,))},
-        )
+      serializer = PostSerializer(data=post_data)
+      serializer.is_valid(raise_exception=True)
+      post = serializer.save()
+
 
     return HttpResponseNotAllowed(["GET", "POST"])
 
@@ -45,15 +43,13 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
+      return JsonResponse(PostSerializer(post).data)
     elif request.method == "PUT":
-        post_data = json.loads(request.body)
-        for field, value in post_data.items():
-            setattr(post, field, value)
-        post.save()
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
+      serializer = PostSerializer(post, data=post_data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return HttpResponse(status=HTTPStatus.NO_CONTENT)
     elif request.method == "DELETE":
         post.delete()
         return HttpResponse(status=HTTPStatus.NO_CONTENT)
-
     return HttpResponseNotAllowed(["GET", "PUT", "DELETE"])
