8057cf114156bca19a28687996bb7d77dec3ea11
Thiruvalluvan Murugesan <tiruvaalluvar.m@gmail.com>
2024-01-14 02:41:55 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -17,6 +17,13 @@ from blog.api.serializers import (
 )
 from blog.models import Post, Tag
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
+from rest_framework.exceptions import PermissionDenied
+
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -70,3 +77,35 @@ class TagViewSet(viewsets.ModelViewSet):
             tag.posts, many=True, context={"request": request}
         )
         return Response(post_serializer.data)
+
+
+@method_decorator(cache_page(300))
+@method_decorator(vary_on_headers("Authorization"))
+@method_decorator(vary_on_cookie)
+@action(methods=["get"], detail=False, name="Posts by the logged in user")
+def mine(self, request):
+  if request.user.is_anonymous:
+    raise PermissionDenied("You must be logged in to see which Posts are yours")
+  posts = self.get_queryset().filter(author=request.user)
+  serializer = PostSerializer(posts, many=True, context={"request": request})
+  return Response(serializer.data)
+
+
+
+@method_decorator(cache_page(120))
+def list(self, *args, **kwargs):
+  return super(PostViewSet, self).list(*args, **kwargs)
+
+@method_decorator(cache_page(300))
+def get(self, *args, **kwargs):
+  return super(UserDetail, self).get(*args, *kwargs)
+
+
+@method_decorator(cache_page(300))
+def list(self, *args, **kwargs):
+  return super(TagViewSet, self).list(*args, **kwargs)
+
+@method_decorator(cache_page(300))
+def retrieve(self, *args, **kwargs):
+  return super(TagViewSet, self).retrieve(*args, **kwargs)
+
