c762103a67c52589dc37c3170e043e8c4c067f45
Thiruvalluvan Murugesan <tiruvaalluvar.m@gmail.com>
2024-01-14 11:10:40 +0000

    Finish throttling

---
+++ blog/api/throttling.py
@@ -0,0 +1,17 @@
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+
+class AnonSustainedThrottle(AnonRateThrottle):
+  scope = "anon_sustained"
+
+
+class AnonBurstThrottle(AnonRateThrottle):
+  scope = "anon_burst"
+
+
+class UserSustainedThrottle(UserRateThrottle):
+  scope = "user_sustained"
+
+
+class UserBurstThrottle(UserRateThrottle):
+  scope = "user_burst"
